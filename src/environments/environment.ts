3059c1057334389088d47ae51c074a2dbc53a670
David Tabaka <DavidATabaka@gmail.com>
2023-09-21 16:20:58 -0400

    ui updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: "AIzaSyCRxebCj12lI3bt8ve5GUhkBTbDqpLEHDo",
-      authDomain: "gcp-developer-1.firebaseapp.com",
-      projectId: "gcp-developer-1",
-      storageBucket: "gcp-developer-1.appspot.com",
-      messagingSenderId: "103566812227",
-      appId: "1:103566812227:web:cdb321537c2e4f9412e0f6"
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     },
   };
   
